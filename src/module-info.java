b3e5ca226fd603f186bb34c8c9943dd3bc552b8a
Pedromontesi <joaopedromonteiro090@hotmail.com>
2023-11-10 20:22:56 -0300

    Sexta aula de JaxaFX

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+module javafxaula {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.base;
+
+	opens gui to javafx.fxml;
+
+	opens application to javafx.graphics, javafx.fxml;
+}
